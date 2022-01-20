12550c55cf62fcbf517715fd7ed29e8852bfad8e
Battle420 <battlenationyt@gmail.com>
2022-01-20 21:10:25 +0100

    Better lsp compability
    Some code taken from https://github.com/hackorum/nfs

--- lua/plugoptions/cmpconf.lua
+++ lua/plugoptions/cmpconf.lua
@@ -1,77 +1,65 @@
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
-local lspconfig = require('lspconfig')
-
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = {'pyright', 'html' }
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    -- on_attach = my_custom_on_attach,
-    capabilities = capabilities,
-  }
+vim.g.completeopt = "menu,menuone,noselect,noinsert"
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0
+             and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s")
+             == nil
 end
 
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+-- Setup nvim-cmp.
 local cmp = require 'cmp'
-cmp.setup {
+local lspkind = require('lspkind')
+
+cmp.setup({
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
+      -- For `vsnip` user.
+     -- vim.fn["vsnip#anonymous"](args.body)
+
+      -- For `luasnip` user.
+       require('luasnip').lsp_expand(args.body)
+
+      -- For `ultisnips` user.
+      -- vim.fn["UltiSnips#Anon"](args.body)
+    end
   },
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = function(fallback)
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+      elseif vim.fn["vsnip#available"]() == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
       else
-        fallback()
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
-    end,
-    ['<S-Tab>'] = function(fallback)
+    end, {"i", "s"}),
+
+    ["<S-Tab>"] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
       end
-    end,
+    end, {"i", "s"})
   },
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    {name = 'nvim_lsp'}, -- For vsnip user.
+   -- {name = 'vsnip'}, -- For luasnip user.
+     { name = 'luasnip' },
+    -- For ultisnips user.
+    -- { name = 'ultisnips' },
+    {name = 'buffer'}
   },
-}
-local lspkind = require('lspkind')
-cmp.setup {
-  formatting = {
-    format = lspkind.cmp_format({
-      with_text = false, -- do not show text alongside icons
-      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+  formatting = {format = lspkind.cmp_format({with_text = true, maxwidth = 50})}
+})
 
-      -- The function below will be called before any actual modifications from lspkind
-      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-      before = function (entry, vim_item)
-
-        return vim_item
-      end
-    })
-  }
-}
--- lua/plugoptions/lsp/lspinstallerconf.lua
+++ lua/plugoptions/lsp/lspinstallerconf.lua
@@ -3,8 +3,19 @@ local lsp_installer = require("nvim-lsp-installer")
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-    local opts = {}
-
+    local opts = {capabilities= capabilities}
+if server.name == "sumneko_lua" then
+    opts = vim.tbl_deep_extend("force", {
+      settings = {
+        Lua = {
+          runtime = {version = 'LuaJIT', path = vim.split(package.path, ';')},
+          diagnostics = {globals = {'vim','use'}},
+          workspace = {library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false},
+          telemetry = {enable = false}
+        }
+      }
+}, opts)
+end
 
     -- (optional) Customize the options passed to the server
     -- if server.name == "tsserver" then
